c23e7e1f826925cc22b40d9b05ef0381da27ec53
Kiersis <kieranthoms1@gmail.com>
2026-03-23 22:37:58 -0400

    completed steps 6, 7, and 8. created fetch function, converted response object to json, output response json to console

--- src/js/json.js
+++ src/js/json.js
@@ -10,11 +10,11 @@ const section = document.querySelector('section');
         // STEP 5: Use the new URL to create a new request object
     const request = new Request(requestURL);
         // STEP 6: Make a network request with the fetch() function, which returns a Response object
-
+    const response = await fetch(request);
         // STEP 7: Capture the returned Response object and covert to a JSON object using json()
-
+    const iScream = response.json();
         // STEP 8: Output the iScream JSON object to the console
-
+    console.log(iScream);
         // STEP 9a: Invoke the populateHeader function here, then build it below
 
         // STEP 10a: Invoke the showTopFlavors function here, then build it below
